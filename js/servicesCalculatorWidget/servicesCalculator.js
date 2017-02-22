b7027387d78ad47440db954bb445d777a1b32c55
Oleh Zaichenko <oleh.zaichenko@gypost.com>
2017-02-22 16:57:58 +0200

    overflow logic changes

--- js/servicesCalculatorWidget/servicesCalculator.js
+++ js/servicesCalculatorWidget/servicesCalculator.js
@@ -5,9 +5,9 @@
     angular.module('servicesCalculator', []);
 
     angular.module('servicesCalculator').directive('servicesCalculator', [
-        '$document',
+        '$timeout',
         '$http',
-        function ($document, $http) {
+        function ($timeout, $http) {
             return {
                 templateUrl: './js/servicesCalculatorWidget/servicesCalculator.html',
                 restrict: 'AE',
@@ -23,6 +23,8 @@
                     scope.getProgressValue = getProgressValue;
                     scope.selectSanitise = selectSanitise;
                     scope.calcTotalSum = calcTotalSum;
+                    scope.outBudget = false;
+                    scope.topRange = 0;
 
                     $http({
                         method: 'GET',
@@ -51,7 +53,14 @@
                     function getProgressValue() {
                         var totalSpent = calcTotalSum();
 
-                        var result = parseInt(totalSpent / scope.budget * 100);
+                        if(totalSpent <= scope.budget) {
+                            var result = parseInt(totalSpent / scope.budget * 100);
+                            scope.topRange = 0;
+                        }
+                        else {
+                            result = 100;
+                            scope.topRange = parseInt((totalSpent - scope.budget) / totalSpent * 100);
+                        }
 
                         if (isNaN(result)) {
                             result = 0;
@@ -60,35 +69,46 @@
                         return result
                     }
 
+                    function flashOutBudgetLimit() {
+                        scope.outBudget = true;
+                        $timeout(function() {
+                            scope.outBudget = false;
+                        }, 500)
+                    }
+
                     function numberSanitise(index) {
 
                         if(isNaN(scope.list[index].value) || +scope.list[index].value < 0) {
                             scope.list[index].value = 0;
                         }
 
-                        while(calcTotalSum() > scope.budget) {
-                            if(scope.list[index].value > 0) {
-                                scope.list[index].value--;
-                            }
-                            else {
-                                break;
-                            }
+                        if(calcTotalSum() > scope.budget) {
+                            flashOutBudgetLimit();
                         }
+
+                        // while(calcTotalSum() > scope.budget) {
+                        //     if(scope.list[index].value > 0) {
+                        //         scope.list[index].value--;
+                        //     }
+                        //     else {
+                        //         break;
+                        //     }
+                        // }
 
                     }
 
                     function selectSanitise(index) {
-                        if(calcTotalSum() > scope.budget) {
-                            scope.list[index].value = '0';
-                        }
+                        // if(calcTotalSum() > scope.budget) {
+                        //     scope.list[index].value = '0';
+                        // }
                     }
 
                     function budgetSanitise() {
-                        if(calcTotalSum() > scope.budget) {
-                            scope.list.forEach(function(item) {
-                                item.value = 0;
-                            });
-                        }
+                        // if(calcTotalSum() > scope.budget) {
+                        //     scope.list.forEach(function(item) {
+                        //         item.value = 0;
+                        //     });
+                        // }
                     }
 
                     function calcTotalSum() {
